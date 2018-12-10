60cfc10a4af71f88a291f659df22cb0ecfef14b7
Mike <m68476521.1@gmail.com>
2018-12-09 20:58:55 -0800

    Create a simple animation Set a simple Fade animation from 0 to 1 with a 5 sec duration
    by m68476521

--- App.js
+++ App.js
@@ -7,7 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, AdsManagerStatus} from 'react-native';
+import {Platform, StyleSheet, Text, View, AdsManagerStatus, Animated} from 'react-native';
+
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0), //Inital value for opacity 0
+  }
+
+  componentDidMount() {
+    Animated.timing(  //Animated over time
+      this.state.fadeAnim, { // the animated view to drive
+        toValue: 1, // animated to opacicty 1 opaque
+        duration: 5000, //make it take a whilw
+      }
+    ).start(); //start animation
+  }
+
+  render() {
+    let {fadeAnim} = this.state;
+
+    return (
+      <Animated.View
+        style={{...this.props.style,
+        opacity: fadeAnim,}}
+        >
+        {this.props.children}
+        </Animated.View>
+    );
+  }
+}
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,20 +44,16 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+export default class App extends React.Component {
   render() {
     return (
-      <View accessible={true} style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text>Hacker and Looper, Page Post Engagement</Text>
-        <Text>29,967 Post Engagements</Text>
-        {/* <AdsManagerStatus
-          accessibilityLabel={'Status ' + this.props.status}
-          status={this.props.status}
-        /> */}
+     <View style={{flex: 1,
+      alignItems:'center',
+      justifyContent: 'center'}}>
+      <FadeInView style={{width:250, height: 50, backgroundColor: 'powderblue'}}>
+        <Text style={{fontSize:28, textAlign: 'center', margin: 10}}>Fading in!</Text>
+      </FadeInView>
       </View>
     );
   }
